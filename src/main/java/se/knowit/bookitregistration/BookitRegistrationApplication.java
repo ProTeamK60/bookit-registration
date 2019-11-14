2c970778163e4b6180160b8e6e8f7b722c533625
chrvaz <christian.vazques@knowit.se>
2019-11-14 14:44:47 +0100

    initial commit

---
+++ src/main/java/se/knowit/bookitregistration/BookitRegistrationApplication.java
@@ -0,0 +1,13 @@
+package se.knowit.bookitregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookitRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookitRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/knowit/bookitregistration/BookitRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package se.knowit.bookitregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookitRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
