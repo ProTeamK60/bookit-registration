edbb9c46e66f3a0a87793f3536e57735c8790642
ola.mattsson <ola.mattsson@gmail.com>
2019-11-19 13:39:43 +0100

    Reformatted

--- src/main/java/se/knowit/bookitregistration/dto/RegistrationMapper.java
+++ src/main/java/se/knowit/bookitregistration/dto/RegistrationMapper.java
@@ -10,26 +10,25 @@ public class RegistrationMapper {
         if (notNullOrBlank(dto.getEventId())) {
             registration.setEventId(UUID.fromString(dto.getEventId()));
         }
-        if (notNullOrBlank(dto.getRegistrationId())){
+        if (notNullOrBlank(dto.getRegistrationId())) {
             registration.setRegistrationId(UUID.fromString(dto.getRegistrationId()));
         }
         registration.setEmail(dto.getEmail());
         return registration;
     }
-
+    
     public RegistrationDTO toDTO(Registration model) {
         RegistrationDTO dto = new RegistrationDTO();
         if (model.getEventId() != null) {
             dto.setEventId(model.getEventId().toString());
         }
-        if(model.getRegistrationId() != null)
-        {
+        if (model.getRegistrationId() != null) {
             dto.setRegistrationId(model.getRegistrationId().toString());
         }
         dto.setEmail(model.getEmail());
         return dto;
     }
-
+    
     private boolean notNullOrBlank(String test) {
         return test != null && !test.isBlank();
     }
