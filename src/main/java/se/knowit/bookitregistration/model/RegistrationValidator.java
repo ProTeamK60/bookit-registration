647b6a065f8b1e8133b2868bb91d77a1acded5e4
ola.mattsson <ola.mattsson@gmail.com>
2019-11-19 13:41:24 +0100

    Reformatted

--- src/main/java/se/knowit/bookitregistration/model/RegistrationValidator.java
+++ src/main/java/se/knowit/bookitregistration/model/RegistrationValidator.java
@@ -3,22 +3,24 @@ package se.knowit.bookitregistration.model;
 import java.util.Objects;
 
 public class RegistrationValidator {
-
-    public Registration ensureRegistrationIsValidOrThrowException(Registration incomingRegistration)
-    {
+    
+    public Registration ensureRegistrationIsValidOrThrowException(Registration incomingRegistration) {
         Registration registration = Objects.requireNonNull(incomingRegistration, "Registration must not be null.");
         ensureEmailIsValid(registration);
         ensureEventIdIsValid(registration);
         return registration;
     }
-
-    private void ensureEventIdIsValid(Registration registration)
-    {
-        if(registration.getEventId() == null) throw new NullPointerException();
+    
+    private void ensureEventIdIsValid(Registration registration) {
+        if (registration.getEventId() == null) {
+            throw new NullPointerException();
+        }
     }
-
+    
     private void ensureEmailIsValid(Registration registration) {
         String email = registration.getEmail();
-        if (email == null || email.isBlank() || !email.matches("^(.+)@(.+)$")) throw new IllegalArgumentException("Invalid email: " + email);
+        if (email == null || email.isBlank() || !email.matches("^(.+)@(.+)$")) {
+            throw new IllegalArgumentException("Invalid email: " + email);
+        }
     }
 }
